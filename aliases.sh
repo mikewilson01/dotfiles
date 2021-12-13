0a6773bb6e63e67875c0f6759499776255a40f2b
Mike Wilson <michael.wilson@shopify.com>
2021-12-13 14:43:08 -0800

    Setup

---
+++ aliases.sh
@@ -0,0 +1,5 @@
+hash gls >/dev/null 2>&1 || alias gls="ls"
+
+alias lsd='ls -l | grep "^d"'
+
+alias fs="stat -f \"%z bytes\""
---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Link dotfiles
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
