b4e536b2d1499bea6e4fca3a59cec29a9199448f
Mike Wilson <michael.wilson@shopify.com>
2021-12-13 14:45:09 -0800

    Another change

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@
 # Link dotfiles
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+
+
+# Some aliases
+alias lsd='ls -l | grep "^d"'
+
+alias fs="stat -f \"%z bytes\""
